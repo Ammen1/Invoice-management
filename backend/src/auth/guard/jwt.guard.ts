d709f1bb81ba94e0ae069549519a6e352982192b
Tamirat <amenguda@gmail.com>
2024-07-31 13:52:39 +0300

    Enhance JwtGuard with logging for access attempts- Add logging functionality to JwtGuard using NestJS Logger- Log HTTP method and URL on access attempts- Log warnings for unauthorized access attempts with detailed messages- Improve debugging and monitoring capabilities

---
+++ backend/src/auth/guard/jwt.guard.ts
@@ -0,0 +1,35 @@
+import { AuthGuard } from "@nestjs/passport";
+import {
+  ExecutionContext,
+  Injectable,
+  Logger,
+  UnauthorizedException,
+} from "@nestjs/common";
+import { Reflector } from "@nestjs/core";
+
+@Injectable()
+export class JwtGuard extends AuthGuard("jwt") {
+  private readonly logger = new Logger(JwtGuard.name);
+
+  constructor(private reflector: Reflector) {
+    super();
+  }
+
+  canActivate(context: ExecutionContext) {
+    const request = context.switchToHttp().getRequest();
+    this.logger.log(`Attempting access to ${request.method} ${request.url}`);
+    return super.canActivate(context);
+  }
+
+  handleRequest(err, user, info, context) {
+    if (err || !user) {
+      const request = context.switchToHttp().getRequest();
+      const message = err ? err.message : info ? info.message : "Unauthorized";
+      this.logger.warn(
+        `Unauthorized access attempt to ${request.method} ${request.url}: ${message}`,
+      );
+      throw err || new UnauthorizedException();
+    }
+    return user;
+  }
+}
