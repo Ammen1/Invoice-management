0aef3273b3390e18072d93bcf740b19241fc7f57
Tamirat <amenguda@gmail.com>
2024-07-31 14:58:07 +0300

    Add ConfigModule and organize modules in AppModule- ConfigModule is now globally available with `isGlobal: true`.- Imported and integrated AuthModule, UserModule, and PrismaModule into AppModule.

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,13 +1,18 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
-import { InvoiceModule } from './invoice/invoice.module';
-import { PrismaModule } from './prisma/prisma.module';
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+
+import { AuthModule } from "./auth/auth.module";
+import { UserModule } from "./user/user.module";
+import { PrismaModule } from "./prisma/prisma.module";
 
 @Module({
-  imports: [UserModule, InvoiceModule, PrismaModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    AuthModule,
+    UserModule,
+    PrismaModule,
+  ],
 })
 export class AppModule {}
