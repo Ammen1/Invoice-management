0f632ea2fed8bb22f1f54a391be82b5cdb234d47
Tamirat <amenguda@gmail.com>
2024-07-31 13:23:49 +0300

    Add PrismaService for database interaction- Extend PrismaClient to create PrismaService- Configure PrismaClient using database URL from ConfigService- Implement cleanDb method to delete all users

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -1,4 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { PrismaClient } from "@prisma/client";
 
 @Injectable()
-export class PrismaService {}
+export class PrismaService extends PrismaClient {
+  constructor(config: ConfigService) {
+    super({
+      datasources: {
+        db: {
+          url: config.get<string>("DATABASE_URL"),
+        },
+      },
+    });
+  }
+
+  async cleanDb() {
+    // Use a transaction to ensure all deletions happen atomically
+    await this.$transaction([this.user.deleteMany()]);
+  }
+}
